083ec41b5d0f6f59f859d2d0e3f5f3707cccb894
MASHIRO\Mashiro <youho98zzz@gmail.com>
2023-05-12 21:06:33 +0900

    パーティーメンバーリストを作ってみました。

---
+++ Assets/Scripts/PartyMembers.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyMembers : MonoBehaviour
+{
+    private void Start()
+    {
+        List<string> questPartyMembers = new List<string>()
+        {
+            "Grim the Barbarian",
+            "Merlin the Wise",
+            "Sterling the Knight"
+        };
+        
+        //Debug.LogFormat("Party Members: {0}", questPartyMembers.Count);
+        foreach (var member in questPartyMembers)
+        {
+            Debug.LogFormat("Member:{0}", member);
+        }
+    }
+}
