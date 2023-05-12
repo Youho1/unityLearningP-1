4213f4d09dc26336c6709e1a53a25615cc363c49
MASHIRO\Mashiro <youho98zzz@gmail.com>
2023-05-13 02:55:26 +0900

    Weaponクラスを追加しました。

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+public class Weapon 
+{
+    public string _name;
+    public int _damage;
+
+    public Weapon(string name, int damage)
+    {
+        _name = name;
+        _damage = damage;
+    }
+
+    public void PrintWeaponStatus()
+    {
+        Debug.LogFormat("Weapon: {0} - {1} DMB", _name, _damage);
+    }
+}
